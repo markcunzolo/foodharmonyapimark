210110bf2600d2c7b8e8266787c273f4126e23cb
Shaun Connolly <shaunconnolly@Shauns-MBP.fios-router.home>
2016-08-23 21:00:53 -0400

    app

--- config/application.rb
+++ config/application.rb
@@ -8,16 +8,32 @@ Bundler.require(*Rails.groups)
 
 module FoodApp
   class Application < Rails::Application
-
-    config.middleware.insert_before 0, "Rack::Cors" do
+    config.middleware.insert_before 0, "Rack::Cors", :debug => true, :logger => (-> { Rails.logger }) do
       allow do
         origins '*'
+
+        resource '/cors',
+                 :headers => :any,
+                 :methods => [:post],
+                 :credentials => true,
+                 :max_age => 0
+
         resource '*',
                  :headers => :any,
                  :expose  => ['access-token', 'expiry', 'token-type', 'uid', 'client'],
                  :methods => [:get, :post, :put, :patch, :delete, :options]
       end
     end
+
+    # config.middleware.insert_before 0, "Rack::Cors" do
+    #   allow do
+    #     origins '*'
+    #     resource '*',
+    #              :headers => :any,
+    #              :expose  => ['access-token', 'expiry', 'token-type', 'uid', 'client'],
+    #              :methods => [:get, :post, :put, :patch, :delete, :options]
+    #   end
+    # end
     config.active_record.raise_in_transactional_callbacks = true
     # config.api_only = false
     # config.assets.initialize_on_precompile = false
