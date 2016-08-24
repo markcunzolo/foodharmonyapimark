1006d19659f2807f0ca9a1cb4f59555441b3a396
Shaun Connolly <shaunconnolly@Shauns-MBP.fios-router.home>
2016-08-23 20:55:07 -0400

    application rb changes

--- config/application.rb
+++ config/application.rb
@@ -19,7 +19,7 @@ module FoodApp
       end
     end
     config.active_record.raise_in_transactional_callbacks = true
-    config.api_only = false
-    config.assets.initialize_on_precompile = false
+    # config.api_only = false
+    # config.assets.initialize_on_precompile = false
   end
 end
