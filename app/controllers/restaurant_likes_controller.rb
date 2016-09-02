9cab32c86c8318263a9f518d0f08c0bbd4a3e24f
Shaun Connolly <shaunconnolly@Shauns-MBP.fios-router.home>
2016-09-01 21:17:47 -0400

    Like fix

--- app/controllers/restaurant_likes_controller.rb
+++ app/controllers/restaurant_likes_controller.rb
@@ -1,8 +1,14 @@
 class RestaurantLikesController < ApplicationController
 
   def create
-    @restaurant_like = RestaurantLike.new(new_restaurant_like_params)
-    @restaurant_like.save!
+    @existing_like = RestaurantLike.where(:restaurant_id => new_restaurant_like_params[:restaurant_id]).where(:user_id => new_restaurant_like_params[:user_id]).first
+    if @existing_like.present?
+      RestaurantLike.where(:restaurant_id => new_restaurant_like_params[:restaurant_id]).where(:user_id => new_restaurant_like_params[:user_id]).update_all(:liked => new_restaurant_like_params[:liked], :disliked => new_restaurant_like_params[:disliked])
+    else
+      @restaurant_like = RestaurantLike.new(new_restaurant_like_params)
+      @restaurant_like.save!
+    end
+
     render json: Restaurant.all.as_json(:include => [:types, :genres, :restaurant_likes])
   end
 
